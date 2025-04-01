bf7d225f92b81afa0762afce013a3519d07208f0
Binni-pooh <118846261+Binni-pooh@users.noreply.github.com>
2025-04-01 17:54:49 +0200

    Update config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -2,5 +2,5 @@ import * as process from "node:process";
 
 export const config = {
     port: process.env.PORT || 3100,
-    databaseUrl: process.env.MONGODB_URI || "mongodb+srv://maxc0766:4a8VV5my3MpMnA3A@aplikacjewebowe.sn3jxxw.mongodb.net/IoT?retryWrites=true&w=majority&appName=AplikacjeWebowe"
+    databaseUrl: process.env.MONGODB_URI || "mongodb+srv://@aplikacjewebowe.sn3jxxw.mongodb.net/IoT?retryWrites=true&w=majority&appName=AplikacjeWebowe"
 };
